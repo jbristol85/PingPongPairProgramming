dc08e483a05f8f3853b96ac71460323300f158d0
jbristol85 <josh.bristol2@gmail.com>
2018-07-24 17:42:23 -0400

    Add project files.

---
+++ PingPongPairProgramming/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PingPongPairProgramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
